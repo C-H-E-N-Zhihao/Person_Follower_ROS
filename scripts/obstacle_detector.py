eaa6bb87279c469f59ca66414b7fee4918cdbbf7
Zhihao Chen <184526868+C-H-E-N-Zhihao@users.noreply.github.com>
2025-06-05 16:21:38 +0200

    code formatted

--- scripts/obstacle_detector.py
+++ scripts/obstacle_detector.py
@@ -7,7 +7,6 @@ from sensor_msgs.msg import LaserScan
 class ObstacleDetector:
     def __init__(self):
         self.obstacle_front = False
-        self.min_distance = 1 # threshold distance in meters
         self.laser_sub = rospy.Subscriber("/scan", LaserScan, self.scan_callback)
 
     def angle_to_index(self, angle_deg, scan_msg):
@@ -21,26 +20,23 @@ class ObstacleDetector:
             index = len(scan_msg.ranges) - 1
         return index
 
-    def check_obstacle_in_sector(self, scan_msg, start_deg, end_deg):
+    def check_obstacle_in_sector(self, scan_msg, start_deg, end_deg, min_distance=1):
         """Check if there is an obstacle within min_distance in angle sector."""
         start_idx = self.angle_to_index(start_deg, scan_msg)
         end_idx = self.angle_to_index(end_deg, scan_msg)
         for i in range(start_idx, end_idx + 1):
             dist = scan_msg.ranges[i]
-            if dist > 0 and dist < self.min_distance:
+            if dist > 0 and dist < min_distance:
                 return True
         return False
 
     def scan_callback(self, msg):
-        # Check obstacle in front sector (around ±20 degrees)
-        self.obstacle_front = self.check_obstacle_in_sector(msg, 340, 359) or \
-                   self.check_obstacle_in_sector(msg, 0, 20)
-
-        # Check obstacle in left sector (around ±20 degrees)
-        self.obstacle_left = self.check_obstacle_in_sector(msg, 70, 110)
+        """Store latest LaserScan data."""
+        self.latest_scan = msg
 
     def is_obstacle_front(self):
-        return self.obstacle_front
+        return self.check_obstacle_in_sector(self.latest_scan, 340, 359, 1) or \
+               self.check_obstacle_in_sector(self.latest_scan, 0, 20, 1)
 
     def is_obstacle_left(self):
-        return self.obstacle_left
+        return self.check_obstacle_in_sector(self.latest_scan, 70, 110, 0.5)
